f2e44d0362e709f5b41263697acce8e09a199aef
kitet <kitetnet@gmail.com>
2017-06-15 17:22:56 +0300

    navigation to edit question section now happens only when user 'owns' the question

--- app/routes/question.js
+++ app/routes/question.js
@@ -6,41 +6,42 @@ handles new answer compose and saving through to the firebase
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	ownsQuestion: false,
-	session: Ember.inject.service(),
-	model(obj){
-		return Ember.RSVP.hash({
-			question: this.store.findRecord('question', obj.question_id),
-		});
-	},
-	afterModel(){
-		
-	},
-	actions:{
-		saveAns(params){
-			var  newAns=this.store.createRecord('answer',params);
-			var question=params.question;
-			question.get('answers').addObject(newAns);
-			newAns.save().then(function(){
-				return question.save();
-			});
-			this.transitionTo('question');
-		},
-		editQuestion(question){
-			// this.store.findRecord('user','-KmfVScJw-1qJM8DF3DZ').then(function(user){
-			// 	console.log(user);
-			// });
-			var emailinsession=this.get('session.currentUser.email');
-			var emailassoc=question.get('user').then((user)=>{
-				return user.get('ema');
-			});
-			if(emailinsession=emailassoc){
-				console.log('can edit');
-			}
-			else{
+    ownsQuestion: false,
+    session: Ember.inject.service(),
+    model(obj) {
+        return Ember.RSVP.hash({
+            question: this.store.findRecord('question', obj.question_id),
+        });
+    },
+    afterModel() {
 
-			}
-			//this.transitionTo('editquestion', question.id);
-		}
-	}
+    },
+    actions: {
+        saveAns(params) {
+            var newAns = this.store.createRecord('answer', params);
+            var question = params.question;
+            question.get('answers').addObject(newAns);
+            newAns.save().then(function() {
+                return question.save();
+            });
+            this.transitionTo('question');
+        },
+        editQuestion(question) {
+        	var self=this;
+            // this.store.findRecord('user','-KmfVScJw-1qJM8DF3DZ').then(function(user){
+            // 	console.log(user);
+            // });
+            var emailinsession = this.get('session.currentUser.email');
+            var check = question.get('user').then((userobj) => {
+                var x = userobj.get('ema');
+                if (emailinsession == x) {
+                    console.log(true);
+                    self.transitionTo('editquestion', question.id);
+                } else {
+                	console.log(false);
+                    return false;
+                }
+            });
+        }
+    }
 });
