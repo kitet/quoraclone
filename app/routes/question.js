6a078f907df2084dffc913b23338fb7986c26814
kitet <kitetnet@gmail.com>
2017-06-15 18:33:12 +0300

    updated editquestion action on question route to check for authentication status

--- app/routes/question.js
+++ app/routes/question.js
@@ -6,7 +6,7 @@ handles new answer compose and saving through to the firebase
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	showButton:false,
+    showButton: false,
     session: Ember.inject.service(),
     model(obj) {
         return Ember.RSVP.hash({
@@ -24,18 +24,22 @@ export default Ember.Route.extend({
             this.transitionTo('question');
         },
         editQuestion(question) {
-        	var self=this;
-            var emailinsession = this.get('session.currentUser.email');
-            var check = question.get('user').then((userobj) => {
-                var x = userobj.get('ema');
-                if (emailinsession == x) {
-                    //console.log(true);
-                    self.transitionTo('editquestion', question.id);
-                } else {
-                	alert('Trying to update question posted by someone else not allowed');
-                    return false;
-                }
-            });
+            var self = this;
+            if (this.get('session.isAuthenticated')) {
+                var emailinsession = this.get('session.currentUser.email');
+                var check = question.get('user').then((userobj) => {
+                    var x = userobj.get('ema');
+                    if (emailinsession == x) {
+                        //console.log(true);
+                        self.transitionTo('editquestion', question.id);
+                    } else {
+                        alert('Trying to update question posted by someone else not allowed');
+                        return false;
+                    }
+                });
+            } else {
+            	alert('Log in to update');
+            }
         }
     }
 });
