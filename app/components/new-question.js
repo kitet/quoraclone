3c4f490746f12ab38629c26cd14a146c2f10cb75
kitet <kitetnet@gmail.com>
2017-06-16 14:36:30 +0300

    began using modal on the question

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,40 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	showF:false,
-	session: Ember.inject.service(),
-	actions:{
-		saveQ(){
-			var params={
-				author: this.get('author'),
-				category: this.get('category'),
-				content: this.get('content'),
-				note: this.get('note'),
-				tag: this.get('tag'),
-				timestamp: new Date().getTime(),
-				user:this.get('user')
-			}
-			this.sendAction('saveQuestion', params);
-			this.set('showF', false);
-			this.set('author',"");
-			this.set('category',"");
-			this.set('content',"");
-			this.set('note',"");
-			this.set('tag',"");
-		},
-		showForm(){
-			//var checkUser=this.get('session');
-			//console.log('skgj');
-			//this.set('showF',true);
-			this.sendAction('askQuestion');
-		},
-		cancel(){
-			this.sendAction('cancelQuestion');
-		},
-		signIn(){
-			//redirect to sign in page
-			this.sendAction('signIn',);
-		}
+    showF: false,
+    session: Ember.inject.service(),
+    actions: {
+        saveQ() {
+            var params = {
+                author: this.get('author'),
+                category: this.get('category'),
+                content: this.get('content'),
+                note: this.get('note'),
+                tag: this.get('tag'),
+                timestamp: new Date().getTime(),
+                user: this.get('user')
+            }
+            this.sendAction('saveQuestion', params);
+            this.set('showF', false);
+            this.set('author', "");
+            this.set('category', "");
+            this.set('content', "");
+            this.set('note', "");
+            this.set('tag', "");
+        },
+        showForm() {
+            //var checkUser=this.get('session');
+            //console.log('skgj');
+            //this.set('showF',true);
+            this.sendAction('askQuestion');
+        },
+        cancel() {
+            this.sendAction('cancelQuestion');
+        },
+        signIn() {
+            //redirect to sign in page
+            this.sendAction('signIn', );
+        }
 
-	}
+    }
 });
