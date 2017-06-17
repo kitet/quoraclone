8903152202c5c9492cef7634905f0f38bc350fa8
kitet <kitetnet@gmail.com>
2017-06-17 11:32:29 +0300

    redesigned navbar to new ui/same functionality

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -4,10 +4,11 @@ export default Ember.Route.extend({
     map: Ember.inject.service('google-map'),
     afterModel() {
         //console.log('me');
-        var container = this.$('.map-display');
+        debugger;
+        var container = $('.map-display');
         console.log(container);
         var options = {
-            center: this.get('map').center(1.3005323, 36.7823955),
+            center: this.get('map').center(-1.3005323, 36.7823955),
             zoom: 15
         };
         this.get('map').findMap(container, options);
